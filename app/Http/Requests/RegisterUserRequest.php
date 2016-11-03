358cd68215ec92948ac85d8acb39e0a7051929f0
Mozartted <mozart.osita@gmail.com>
2016-11-03 21:38:42 +0100

    RegistrationValidation Request for checking that an input is valid and Unique.

---
+++ app/Http/Requests/RegisterUserRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+
+        $timestamp = strtotime('-15 years');
+
+        return [
+            'firstname'				=>	'required|min:2|alpha',
+            'lastname'				=>	'required|min:2|alpha',
+            'email'					=> 	'required|email|unique:users',
+            'username'              =>  'required|alpha|numeric|unique:users',
+            'password'				=>	'required|confirmed|between:4,12',
+            'password_confirmation'	=> 	'required',
+            'gender'				=>	'required|alpha|size:1',
+            'month'					=>	'required|numeric|between:01,12',
+            'day'					=>	'required|numeric|between:01,31',
+            'year'					=>	'required|numeric|before:'.date('Y', $timestamp),
+            'profileimage'			=>	'required|image|mimes:jpeg,jpg,bmp,png,gif'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'firstname'=>'First name must be alphabets please',
+            'lastname'=>'last name must be alphabets only',
+        ];
+    }
+
+
+}
