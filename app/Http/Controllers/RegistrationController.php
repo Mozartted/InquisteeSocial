b3dced33325866cf39369fb0498416ea6b2a9cba
Mozartted <mozart.osita@gmail.com>
2016-11-03 20:54:35 +0100

    RegistrationController added to the Controllers

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -4,6 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+/*
+|--------------------------------------------------------------------------
+| Registration Controller
+|--------------------------------------------------------------------------
+|
+| Handles all operation associated with the Registration Routes
+|
+*/
+
 class RegistrationController extends Controller
 {
     //
