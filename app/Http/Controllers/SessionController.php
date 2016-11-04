7dc51f344156d3b183b11ad55394a3a1ada5d006
Mozartted <mozart.osita@gmail.com>
2016-11-04 12:47:27 +0100

    Session Controller to handle login and logouts

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -1,10 +1,56 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Http\Requests\LoginUserRequest;
+use Auth;
 
-use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Session Controller
+|--------------------------------------------------------------------------
+|
+| Handles all activities related to user session login and logout.
+| have fun....
+|
+*/
 
 class SessionController extends Controller
 {
-    //
+    public function store(LoginUserRequest $request){
+        /*
+        |--------------------------------------------------------------------------
+        | Login User
+        |--------------------------------------------------------------------------
+        |
+        | Login user using the Auth Attempt option
+        |
+        */
+        if(! Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+            return redirect()->back()->with('error','we are unable to sign you in, check your credentials please');
+        }else{
+            $CurrentUser=Auth::user();
+            return redirect()->route('feeds_path')->with('message','welcome '
+                .$CurrentUser->profile->first_name.
+                ', How are you?');
+        }
+
+    }
+
+    public function destroy(){
+        /*
+        |--------------------------------------------------------------------------
+        | Logout User
+        |--------------------------------------------------------------------------
+        |
+        | This section does all the logout shit by destroying all sessions and
+        | redirecting user to the registration view of the home page.
+        |
+        */
+        Auth::logout();
+
+        return response()->json(['response' => 'success']);
+
+    }
+
 }
