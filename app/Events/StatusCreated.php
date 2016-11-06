636e85a10be7986bdefa9f755bff0e290a9c3621
Mozartted <mozart.osita@gmail.com>
2016-11-06 08:51:50 +0100

    Events and Listeners, not yet populated

---
+++ app/Events/StatusCreated.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Status;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class StatusCreated implements  ShouldBroadcast
+{
+    use InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+
+
+    protected $status;
+    public function __construct(Status $status)
+    {
+        $this->status=$status;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
---
+++ app/Listeners/StatusCreatedListener.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\StatusCreated;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class StatusCreatedListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  StatusCreated  $event
+     * @return void
+     */
+    public function handle(StatusCreated $event)
+    {
+        //
+    }
+}
