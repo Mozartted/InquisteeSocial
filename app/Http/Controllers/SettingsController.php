943ecca9ad5f96b3ae74cb6f885c36ddcb696b87
Mozartted <mozart.osita@gmail.com>
2016-11-18 17:59:31 +0100

    (Files): Completed Like and Settings Controller

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use Illuminate\Http\Request;
 
 class SettingsController extends Controller
 {
-    //
+    public function accountUpdate(){
+        $user=Auth::user();
+
+        if(isset($request)){
+            if(!empty($request->username)){
+                $user->username=$request->username;
+                $user->save;
+            }
+
+
+            if(!empty($request->email)){
+                $user->email=$request->email;
+            }
+
+            if(!empty($request->password)){
+                $user->password=$request->password;
+            }
+
+            $user->save();
+        }
+
+        $success="Account successfully updated";
+
+        return response()->json($success);
+
+    }
+
+    public function deactivate(){
+        $user=Auth::user();
+
+        $user->activestatus=false;
+
+        Auth::logout();
+        return redirect()->route('registration_path');
+    }
 }
