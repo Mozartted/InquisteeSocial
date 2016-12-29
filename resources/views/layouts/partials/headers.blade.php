fd524d75ffb0d263c327e13ba76e7ba5b1be3b52
Mozartted <mozart.osita@gmail.com>
2016-12-29 20:23:47 +0100

    Added the Jscroll library as alternative to custom infinite scroll.

--- resources/views/layouts/partials/headers.blade.php
+++ resources/views/layouts/partials/headers.blade.php
@@ -8,6 +8,7 @@
 <script src="script/jquery.js"></script>
 {{-- <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.5/jqu‌​ery-ui.min.js"></script> --}}
 <script src="script/materialize.js"></script>
+{{-- <script src="script/jquery.jscroll.min.js"></script> --}}
 {{-- <script src="js/app.js"></script> --}}
 {{-- <script src="script/jquery.autocomplete.js"></script> --}}
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-timeago/1.4.3/jquery.timeago.js"></script>
