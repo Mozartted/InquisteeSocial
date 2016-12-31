8d234367ada3455b65383c547d6976fb86e49cf3
Mozartted <mozart.osita@gmail.com>
2016-12-31 04:46:04 +0100

    Aligning messages for each user

--- resources/views/messages/partials/messageuser.blade.php
+++ resources/views/messages/partials/messageuser.blade.php
@@ -1,9 +1,15 @@
-<div class="tab-content active" id="tab-2">
-    <div class="card-content sections">
-        <div>
-            <ul class="user-message">
-                @include('messages.partials.message')
-            </ul>
-         </div>
+<!--In here the idea is to have the messages outline into tabbed bosxes for each user-->
+@if (isset($MessageCollection))
+  @foreach ($MessageCollection as $key)
+    <div class="tab-content active" id="tab-{{ $key->responder->id }}">
+        <div class="card-content sections">
+            <div>
+                <ul class="user-message">
+                    @include('messages.partials.message',
+                    ['messages'=>$key->messages])
+                </ul>
+             </div>
+        </div>
     </div>
-</div>
+  @endforeach
+@endif
