a6665cbe39c49683bcefdc258d66942bddcb2cc7
Mozartted <mozart.osita@gmail.com>
2016-12-31 04:47:28 +0100

    Aligning Messages

--- resources/views/messages/partials/message.blade.php
+++ resources/views/messages/partials/message.blade.php
@@ -1,7 +1,14 @@
-<li>
-    <div class="message">
-        <span class="message-head">Mozartted <small>-10pm</small></span>
-        <p>A new Idea behind coding works</p>
-    </div>
- </li>
-<div class="divider"></div>   
+@foreach ($messages as $message)
+  <li>
+      <div class="message">
+        <!--Firstcheck if message is from user it another person-->
+        @if ($message->sender==Auth::user()->id)
+          <span class="message-head">You Sent <small> {{ $message->created_at->diffForHumans() }}</small></span>
+        @else
+          <span class="message-head">{{ $message->sender }} <small> {{ $message->created_at->diffForHumans() }}</small></span>
+        @endif
+          <p>A new Idea behind coding works</p>
+      </div>
+   </li>
+  <div class="divider"></div>
+@endforeach
