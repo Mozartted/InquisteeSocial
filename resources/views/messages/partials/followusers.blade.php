769d37339bb5b77d107e057899a028fd349b6748
Mozartted <mozart.osita@gmail.com>
2017-01-08 14:24:42 +0100

    Follow section added

---
+++ resources/views/messages/partials/followusers.blade.php
@@ -0,0 +1,29 @@
+<div class="card sections">
+    <div class="card-content">
+        <div id="right-side-column" class="col-md-3 row">
+            <div class="module roaming-module ">
+                <div class="flex-module">
+                    <div class="big">
+                        <h6>Who to follow</h6>
+                    </div>
+                    <div class="content">
+                        <div class="user-actions not-following not-muting" data-screen-name="XHNews" data-user-id="487118986">
+                            <button type="button" class="small-follow-btn follow-btn btn small follow-button js-recommended-item">
+                                <div class="js-action-follow follow-text action-text">
+                                    <span class="Icon Icon--follow"></span>
+                                    Follow
+                                </div>
+                                <div class="js-action-unfollow following-text action-text">Following</div>
+                                <div class="js-action-unfollow unfollow-text action-text">Unfollow</div>
+                                <div class="block-text action-text">Blocked</div>
+                                <div class="js-action-unblock unblock-text action-text">Unblock</div>
+                                <div class="js-action-unfollow pending-text action-text">Pending</div>
+                                <div class="js-action-unfollow cancel-req-text action-text">Cancel</div>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+</div>
