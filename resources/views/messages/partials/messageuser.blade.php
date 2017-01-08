28053fae7f3b9d024c92daf537ab50a6d3f28478
Mozartted <mozart.osita@gmail.com>
2017-01-08 14:25:19 +0100

    Adjusted for array

--- resources/views/messages/partials/messageuser.blade.php
+++ resources/views/messages/partials/messageuser.blade.php
@@ -1,12 +1,11 @@
 <!--In here the idea is to have the messages outline into tabbed bosxes for each user-->
 @if (isset($MessageCollection))
   @foreach ($MessageCollection as $key)
-    <div class="tab-content active" id="tab-{{ $key->responder->id }}">
+    <div class="tab-content active" id="tab-{{ $key['responder']->id }}">
         <div class="card-content sections">
             <div>
                 <ul class="user-message">
-                    @include('messages.partials.message',
-                    ['messages'=>$key->messages])
+                    @include('messages.partials.message',['messages'=>$key['messages']])
                 </ul>
              </div>
         </div>
