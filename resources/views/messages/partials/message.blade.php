af0b2cfec7bb374afb4b5d0f567910cdfbc423fd
Mozartted <mozart.osita@gmail.com>
2017-01-12 02:16:07 +0100

    Seating the small stuff...

--- resources/views/messages/partials/message.blade.php
+++ resources/views/messages/partials/message.blade.php
@@ -3,11 +3,12 @@
       <div class="message">
         <!--Firstcheck if message is from user it another person-->
         @if ($message->sender==Auth::user()->id)
-          <span class="message-head">You Sent <small> {{ $message->created_at->diffForHumans() }}</small></span>
+          <span>You Sent <small> {{ $message->created_at->diffForHumans() }}</small></span>
         @else
-          <span class="message-head">{{ $message->sender }} <small> {{ $message->created_at->diffForHumans() }}</small></span>
+          <span class="message-head">{{
+             (\App\Models\Message::where('id',$message->id)->first())->sending->username }} <small> {{ $message->created_at->diffForHumans() }}</small></span>
         @endif
-          <p>A new Idea behind coding works</p>
+          <span class="message-head">{{ $message->message }}</span>
       </div>
    </li>
   <div class="divider"></div>
