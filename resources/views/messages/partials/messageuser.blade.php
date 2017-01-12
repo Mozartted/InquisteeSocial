1f4eb14844266b752e5636f64e96d46f6e8669bb
Mozartted <mozart.osita@gmail.com>
2017-01-12 02:15:50 +0100

    Adjusted for recent User :smile:

--- resources/views/messages/partials/messageuser.blade.php
+++ resources/views/messages/partials/messageuser.blade.php
@@ -1,7 +1,14 @@
 <!--In here the idea is to have the messages outline into tabbed bosxes for each user-->
 @if (isset($MessageCollection))
   @foreach ($MessageCollection as $key)
-    <div class="tab-content active" id="tab-{{ $key['responder']->id }}">
+    @if ($recentUser==$key['responder']->id)
+      <div class="tab-content active" id="tab-{{ $key['responder']->id }}" style="overflow-y:scroll">
+    @elseif($resentUser==$key['responder']->id)
+      <div class="tab-content active" id="tab-{{ $key['responder']->id }}" style="overflow-y:scroll">
+    @else
+      <div class="tab-content" id="tab-{{ $key['responder']->id }}" style="overflow-y:scroll">
+    @endif
+
         <div class="card-content sections">
             <div>
                 <ul class="user-message">
