77ef282af17c5e1e1b3b4ab867a46d81d6c719d6
Mozartted <mozart.osita@gmail.com>
2017-01-12 16:20:24 +0100

    Applying the Index path of notifications

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class NotificationsController extends Controller
 {
-    //
+    public function index(){
+      //returning all notifications.
+      return view('notification.index');
+    }
 }
