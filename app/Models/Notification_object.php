9600fc2e8a935287342a895aeca20e58ac9f3800
Mozartted <mozart.osita@gmail.com>
2017-01-13 08:58:46 +0100

    Modified Notification_object, switching timestamps

--- app/Models/Notification_object.php
+++ app/Models/Notification_object.php
@@ -9,11 +9,7 @@ class Notification_object extends Model
     protected $table='notification_objects';
 
     public $timestamps=false;
-    protected $fillable=['notifications_id','object_name'];
-
-    public function notification(){
-        return $this->belongsTo('App\Models\Notification','notifications_id');
-    }
+    protected $fillable=['notifications_id'];
 
     public function notification_changes(){
         return $this->hasMany('App\Models\Notification_change','notification_objects_id');
