2de698a84ef738091ce8ed116f18c56cd8ae95ac
Mozartted <mozart.osita@gmail.com>
2017-01-13 08:57:08 +0100

    Adjusting Notification Change model for model stuff :smile:

--- app/Models/Notification_change.php
+++ app/Models/Notification_change.php
@@ -1,10 +1,29 @@
 <?php
 
+/*
+|--------------------------------------------------------------
+| Notification Changes
+|--------------------------------------------------------------
+| Every notification change belongs to a single notification
+| object.
+|
+*/
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Notification_change extends Model
 {
+  protected $table='notification_changes';
+  protected $fillable=['notification_objects_id','verb','actor','actionOn'];
+
+  public function actor(){
+    return $this->belongsTo('App\Models\User','actor');
+  }
+
+  public function actionOn($model){
+      return $this->belongsTo($model,'actionOn');
+  }
 
 }
